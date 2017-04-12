a24609d8a1fdc4d058cd53f2fa4876f306ba6090
Rachel Byrne <rachelbyrne1994@hotmail.com>
2017-04-12 16:40:13 +0100

    Quiz Score

--- Explorer/Explorer/QuizScore.cs
+++ Explorer/Explorer/QuizScore.cs
@@ -26,7 +26,33 @@ namespace Explorer
             
             TextView tv = FindViewById<TextView>(Resource.Id.score);
             string score = Intent.GetStringExtra("Score");
-            tv.Text = score;
+            tv.Text = score + " / " + 10;
+
+            ImageView image = FindViewById<ImageView>(Resource.Id.medal);
+            if (score.Equals("8") || score.Equals("9") || score.Equals("10")) 
+            {
+                image.SetImageResource(Resource.Drawable.goldmedal);
+            }
+            if(score.Equals("7") && score.Equals("6") && score.Equals("5"))
+            {
+               
+            }
+            if(score.Equals("4") && score.Equals("3") && score.Equals("2"))
+            { }
+
+            Button retake = FindViewById<Button>(Resource.Id.retake);
+            retake.Click += delegate
+            {
+                StartActivity(typeof(Quiz));
+            };
+
+            Button cancel = FindViewById<Button>(Resource.Id.cancel);
+            cancel.Click += delegate
+            {
+                StartActivity(typeof(MainActivity));
+            };
+                
+            
         }
     }
 }
